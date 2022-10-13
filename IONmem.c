2d731fd59663bdda1bf19f20682085cee0b6e8a1
yuhua.lin <yuhua.lin@amlogic.com>
2022-10-13 15:45:27 +0800

    libgdc: update libdewarp.so and modify ION allocation [1/1]
    PD#SWPL-97211
    
    Problem:
    update libdewarp.so and modify ION allocation
    
    Solution:
    complete it
    
    Verify:
    t7c
    
    Signed-off-by: yuhua.lin <yuhua.lin@amlogic.com>
    Change-Id: Ibac5b680c117745795eb536af5b8e0c1c18d800a

--- IONmem.c
+++ IONmem.c
@@ -49,17 +49,20 @@ int ion_mem_alloc_fd(int ion_fd, size_t size, IONMEM_AllocParams *params, unsign
                 for (int i = 0; i != num_heaps; ++i) {
                     D_GDC("heaps[%d].type=%d, heap_id=%d\n", i, heaps[i].type, heaps[i].heap_id);
                     if ((1 << heaps[i].type) == alloc_hmask) {
+                        /* ion-fb heap is only for display */
+                        if (!strncmp("ion-fb", heaps[i].name, 6))
+                            continue;
                         heap_mask = 1 << heaps[i].heap_id;
                         D_GDC("%d, m=%x, 1<<heap_id=%x, heap_mask=%x, name=%s, alloc_hmask=%x\n",
                             heaps[i].type, 1<<heaps[i].type, heaps[i].heap_id, heap_mask, heaps[i].name, alloc_hmask);
-                        break;
+                        ret = ion_alloc_fd(ion_fd, size, 0, heap_mask, flag, &params->mImageFd);
+                        if (ret >= 0)
+                            break;
                     }
                 }
             }
             free(heaps);
-            if (heap_mask)
-                ret = ion_alloc_fd(ion_fd, size, 0, heap_mask, flag, &params->mImageFd);
-            else
+            if (!heap_mask)
                 E_GDC("don't find match heap!!\n");
         } else {
               if (heaps)
@@ -87,8 +90,8 @@ unsigned long ion_mem_alloc(int ion_fd, size_t size, IONMEM_AllocParams *params,
     if (cache_flag) {
         flag = ION_FLAG_CACHED | ION_FLAG_CACHED_NEEDS_SYNC;
     }
-    D_GDC("%s, cache=%d, bytes=%d, legacy_ion=%d, flag=0x%x\n",
-        __func__, cache_flag, size, legacy_ion, flag);
+    D_GDC("%s, cache=%d, legacy_ion=%d, flag=0x%x\n",
+        __func__, cache_flag, legacy_ion, flag);
 
     if (legacy_ion == 1) {
         if (!cache_flag) {
