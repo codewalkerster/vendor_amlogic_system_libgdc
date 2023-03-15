fb6d38a3991e6ac9e86067a36338dd39db14b2b6
Lele Liu <lele.liu@amlogic.com>
2023-03-15 15:16:06 +0800

    Android U: sync google 2022 master [1/1]
    PD#SWPL-115229
    
    Problem:
    sync google master branch 2022 code
    
    Solution:
    sync code back
    
    Verify:
    local
    
    Change-Id: Idda8693856a6523ee7b1792f1d06752462189a3b
    Signed-off-by: Lele Liu <lele.liu@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -14,8 +14,9 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
 	system/memory/libion/kernel-headers/linux/
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_CFLAGS := -Werror
-LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-GPL legacy_proprietary
-LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+LOCAL_LICENSE_KINDS := legacy_by_exception_only legacy_notice SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-FTL SPDX-license-identifier-GPL SPDX-license-identifier-LGPL-2.1 SPDX-license-identifier-MIT legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := by_exception_only notice restricted proprietary by_exception_only
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/LICENSE
 
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
 LOCAL_PROPRIETARY_MODULE := true
@@ -40,9 +41,9 @@ LOCAL_CFLAGS += -g
 LOCAL_CPPFLAGS := -g
 
 LOCAL_MODULE := gdc_test
-LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-GPL legacy_proprietary
-LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
-
+LOCAL_LICENSE_KINDS := legacy_by_exception_only legacy_notice SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-FTL SPDX-license-identifier-GPL SPDX-license-identifier-LGPL-2.1 SPDX-license-identifier-MIT legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := by_exception_only notice restricted proprietary by_exception_only
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/LICENSE
 
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
 LOCAL_PROPRIETARY_MODULE := true
@@ -62,8 +63,7 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH_32 := $(PRODUCT_OUT)/vendor/lib
 LOCAL_MODULE_PATH_64 := $(PRODUCT_OUT)/vendor/lib64
-LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-GPL legacy_proprietary
-LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+
 
 include $(BUILD_PREBUILT)
 
@@ -86,8 +86,9 @@ LOCAL_CFLAGS += -g
 LOCAL_CPPFLAGS := -g
 
 LOCAL_MODULE := dewarp_test
-LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-GPL legacy_proprietary
-LOCAL_LICENSE_CONDITIONS := notice proprietary by_exception_only
+LOCAL_LICENSE_KINDS := legacy_by_exception_only legacy_notice SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-FTL SPDX-license-identifier-GPL SPDX-license-identifier-LGPL-2.1 SPDX-license-identifier-MIT legacy_proprietary
+LOCAL_LICENSE_CONDITIONS := by_exception_only notice restricted proprietary by_exception_only
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/LICENSE
 
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
 LOCAL_PROPRIETARY_MODULE := true
